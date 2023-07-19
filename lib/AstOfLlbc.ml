b2da757df2819844b231291650236ee833f17d56
Jonathan Protzenko <protz@microsoft.com>
2023-07-19 14:24:33 -0700

    wip

--- lib/AstOfLlbc.ml
+++ lib/AstOfLlbc.ml
@@ -4,11 +4,15 @@ module C = struct
   include Charon.LlbcAst
   include Charon.Types
   include Charon.Expressions
+  include Charon.PrimitiveValues
 end
 
 module K = Krml.Ast
 
 module L = Logging
+
+
+(** Environments *)
 
 type env = {
   (* Lookup functions to resolve various id's into actual declarations. *)
@@ -26,8 +30,6 @@ type env = {
   (* For picking pretty names *)
   crate_name: string;
 }
-
-let builtin_slice: K.lident = ["Eurydice"], "slice"
 
 let lookup_typ env (v1: C.type_var_id) =
   let exception Found of int in
@@ -63,10 +65,20 @@ let push_binder env (t: C.var) =
 let push_binders env (ts: C.var list) =
   List.fold_left push_binder env ts
 
+
+(** Small helpers *)
+
+let builtin_slice: K.lident = ["Eurydice"], "slice"
+
+let with_any = K.(with_type TAny)
+
 let string_of_path_elem (p: Charon.Names.path_elem): string =
   match p with
   | Charon.Names.Ident s -> s
   | Disambiguator i -> Charon.Names.Disambiguator.to_string i
+
+
+(** Translation of types *)
 
 let lid_of_name (env: env) (name: Charon.Names.name): K.lident =
   [ env.crate_name ], String.concat "_" (List.map string_of_path_elem name)
@@ -90,7 +102,7 @@ let lid_of_type_decl_id (env: env) (id: C.type_decl_id) =
   let { C.name; _ } = env.get_nth_type id in
   lid_of_name env name
 
-let rec typ_of_ty (env: env) (ty: 'region Charon.Types.region Charon.Types.ty): K.typ =
+let rec typ_of_ty (env: env) (ty: 'region Charon.Types.ty): K.typ =
   match ty with
   | C.Adt (C.AdtId id, _, []) ->
       TQualified (lid_of_type_decl_id env id)
@@ -120,24 +132,151 @@ let rec typ_of_ty (env: env) (ty: 'region Charon.Types.region Charon.Types.ty): 
   | C.Ref (_, t, _) ->
       K.TBuf (typ_of_ty env t, false)
 
+
+(** Translation of expressions (statements, operands, rvalues, places) *)
+
+let binder_of_var (env: env) (l: C.var): K.binder =
+  let name = Option.value ~default:"x" l.name in
+  Krml.Helpers.fresh_binder name (typ_of_ty env l.var_ty)
+
+let rec with_locals (env: env) (t: K.typ) (locals: C.var list) (k: env -> 'a): 'a =
+  match locals with
+  | [] -> k env
+  | l :: locals ->
+      let env = push_binder env l in
+      let b = binder_of_var env l in
+      K.(with_type t (ELet (b, Krml.Helpers.any, with_locals env t locals k)))
+
+let expression_of_place (env: env) (p: C.place): K.expr =
+  let { C.var_id; projection } = p in
+  let e = with_any K.(EBound (lookup env var_id)) in
+  List.fold_right (fun pe e ->
+    match pe with
+    | C.Deref | C.DerefBox ->
+        Krml.Helpers.mk_deref K.TAny e.K.node
+    | Field _ ->
+        failwith "expression_of_place Field"
+    | Offset ofs_id ->
+        with_any K.(EBufSub (e, with_type (TInt SizeT) (EBound (lookup env ofs_id))))
+  ) projection e
+
+let constant_of_scalar_value { C.value; int_ty } =
+  let w = width_of_integer_type int_ty in
+  K.(with_type (TInt w) (EConstant (w, Z.to_string value)))
+
+let expression_of_operand (env: env) (p: C.operand): K.expr =
+  match p with
+  | Copy p
+  | Move p ->
+      expression_of_place env p
+  | Constant (_ty, Scalar sv) ->
+      constant_of_scalar_value sv
+  | Constant (_ty, Bool b) ->
+      K.(with_type TBool (EBool b))
+  | Constant (_ty, Char _) ->
+      failwith "expression_of_operand Char"
+  | Constant (_ty, String _) ->
+      failwith "expression_of_operand String"
+
+let op_of_binop (op: C.binop): Krml.Constant.op =
+  match op with
+  | C.BitXor -> BXor
+  | C.BitAnd -> BAnd
+  | C.BitOr -> BOr
+  | C.Eq -> Eq
+  | C.Lt -> Lt
+  | C.Le -> Lte
+  | C.Ne -> Neq
+  | C.Ge -> Gte
+  | C.Gt -> Gt
+  | C.Div -> Div
+  | C.Rem -> Mod
+  | C.Add -> Add
+  | C.Sub -> Sub
+  | C.Mul -> Mult
+  | C.Shl -> BShiftL
+  | C.Shr -> BShiftR
+
+let expression_of_rvalue (env: env) (p: C.rvalue): K.expr =
+  match p with
+  | Use op ->
+      expression_of_operand env op
+  | Ref (p, _) ->
+      with_any (EAddrOf (expression_of_place env p))
+  | UnaryOp (_, _) ->
+      failwith "expression_of_rvalue UnaryOp"
+  | BinaryOp (op, e1, e2) ->
+      with_any (EApp (with_any (EOp (op_of_binop op)), [
+        expression_of_rvalue e1;
+        expression_of_rvalue e2]))
+  | Discriminant _ ->
+      failwith "expression_of_rvalue Discriminant"
+  | Aggregate (AggregatedTuple, ops) ->
+      with_any (ETuple (List.map (expression_of_operand env) ops))
+  | Aggregate (AggregatedOption (_, _), _) ->
+      failwith "expression_of_rvalue AggregatedOption"
+  | Aggregate (AggregatedAdt (_, _, _, _), _) ->
+      failwith "expression_of_rvalue AggregatedAdt"
+  | Aggregate (AggregatedRange _, _) ->
+      failwith "expression_of_rvalue AggregatedRange"
+  | Aggregate (AggregatedArray _, _) ->
+      failwith "expression_of_rvalue AggregateArray"
+  | Global _ ->
+      failwith "expression_of_rvalue Global"
+  | Len _ ->
+      failwith "expression_of_rvalue Len"
+
+let rec expression_of_raw_statement (env: env) (ret_var: C.var_id) (ret_typ: K.typ) (s: C.raw_statement): K.expr =
+  match s with
+  | Assign (p, rv) ->
+      let p = expression_of_place env p in
+      let rv = expression_of_rvalue env rv in
+      K.with_unit (EAssign (p, rv))
+  | FakeRead _ ->
+      failwith "C.FakeRead"
+  | SetDiscriminant (_, _) ->
+      failwith "C.SetDiscriminant"
+  | Drop _ ->
+      Krml.Helpers.eunit
+  | Assert _ ->
+      failwith "C.Assert"
+  | Call _ ->
+      failwith "C.Call"
+  | Panic ->
+      with_any (EAbort None)
+  | Return ->
+      K.with_type ret_typ K.(EReturn (with_type ret_typ (EBound (lookup env ret_var))))
+  | Break _ ->
+      with_any EBreak
+  | Continue _ ->
+      with_any EContinue
+  | Nop ->
+      failwith "C.Nop"
+  | Sequence (s1, s2) ->
+      with_any (ESequence [
+        expression_of_raw_statement env ret_var ret_typ s1.content;
+        expression_of_raw_statement env ret_var ret_typ s2.content])
+  | Switch (If (op, s1, s2)) ->
+      with_any (EIfThenElse (expression_of_operand env op,
+        expression_of_raw_statement env ret_var ret_typ s1.content,
+        expression_of_raw_statement env ret_var ret_typ s2.content))
+  | Switch (SwitchInt (_, _, _, _)) ->
+      failwith "expression_of_raw_statement SwitchInt"
+  | Switch (Match (_, _, _)) ->
+      failwith "expression_of_raw_statement Match"
+  | Loop s ->
+      with_any (EWhile (Krml.Helpers.etrue,
+        expression_of_raw_statement env ret_var ret_typ s.content))
+
+
+(** Top-level declarations: orchestration *)
+
 let of_declaration_group (dg: 'id C.g_declaration_group) (f: 'id -> 'a): 'a list =
   (* We do not care about recursion as in C, everything is mutually recursive
      thanks to header inclusion. *)
   match dg with
   | NonRec id -> [ f id ]
   | Rec ids -> List.map f ids
-
-let rec with_locals (env: env) (locals: C.var list) (k: env -> K.expr): K.expr =
-  match locals with
-  | [] -> k env
-  | l :: locals ->
-      let env = push_binder env l in
-      (* TODO: take type, surround in ELet, CPS *)
-      with_locals env locals k
-
-let expression_of_raw_statement (env: env) (s: C.raw_statement) (k: env -> K.expr): K.expr =
-  ignore (env, s, k);
-  failwith "TODO: expression_of_raw_statement"
 
 let decls_of_declarations (env: env) (formatter: C.fun_decl -> Charon.PrintGAst.ast_formatter) (d: C.declaration_group): K.decl list =
   match d with
@@ -173,25 +312,25 @@ let decls_of_declarations (env: env) (formatter: C.fun_decl -> Charon.PrintGAst.
             if is_global_decl_body then
               failwith "TODO: C.Fun is_global decl"
             else
-              let { C.type_params; inputs; output ; _ } = signature in
-              let env = push_type_binders env type_params in
+              let env = push_type_binders env signature.C.type_params in
               let name = lid_of_name env name in
-              let output = typ_of_ty env output in
+              (* `locals` contains, in order: special return variable; function arguments;
+                 local variables *)
               let args, locals = Krml.KList.split (arg_count + 1) locals in
               let return_var = List.hd args in
               let args = List.tl args in
-              let inputs = List.map2 (fun t (arg: C.var) ->
+
+              let return_type = typ_of_ty env return_var.var_ty in
+              let arg_binders = List.map (fun (arg: C.var) ->
                 let name = Option.get arg.name in
-                Krml.Helpers.fresh_binder name (typ_of_ty env t)
-              ) inputs args in
+                Krml.Helpers.fresh_binder name (typ_of_ty env arg.var_ty)
+              ) args in
               let env = push_binders env args in
               let body =
-                with_locals env (return_var :: locals) (fun env ->
-                  expression_of_raw_statement env body.content (fun env ->
-                    K.(with_type output (EBound (lookup env return_var.index)))
-                  ))
+                with_locals env return_type (return_var :: locals) (fun env ->
+                  expression_of_raw_statement env return_var.index return_type body.content)
               in
-              K.DFunction (None, [], List.length type_params, output, name, inputs, body)
+              K.DFunction (None, [], List.length signature.C.type_params, return_type, name, arg_binders, body)
       )
   | C.Global _id ->
       failwith "TODO: C.Global"
