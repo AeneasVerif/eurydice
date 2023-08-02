f0fb395380a39c96ba40a04d351b2ffa3ac25efc
Jonathan Protzenko <protz@microsoft.com>
2023-08-02 11:05:13 -0700

    Much cleaner code-gen

--- lib/Cleanup1.ml
+++ lib/Cleanup1.ml
@@ -35,7 +35,7 @@ let remove_assignments = object(self)
     DFunction (cc, flags, n, t, name, bs, peel_lets to_close e)
 
   method! visit_ELet (not_yet_closed, t) b e1 e2 =
-    let close_now_over candidates mk_node =
+    let close_now_over not_yet_closed candidates mk_node =
       let to_close_now = AtomSet.inter candidates (set_of_map_keys not_yet_closed) in
       let bs = List.of_seq (AtomSet.to_seq to_close_now) in
       let bs = List.map (fun atom ->
@@ -50,33 +50,52 @@ let remove_assignments = object(self)
       (Krml.Helpers.nest bs t (with_type t node)).node
     in
 
-    let close_now candidates =
-      close_now_over candidates (fun not_yet_closed ->
-        ELet (b, self#visit_expr_w not_yet_closed e1, self#visit_expr_w not_yet_closed e2))
+    let count e = count_atoms#visit_expr_w () e in
+
+    let recurse_or_close not_yet_closed e =
+      match e.node with
+      | ELet _ ->
+          self#visit_expr_w not_yet_closed e
+      | _ ->
+          with_type e.typ (close_now_over not_yet_closed (count e) (fun not_yet_closed ->
+            (self#visit_expr_w not_yet_closed e).node))
     in
 
-    let count e = count_atoms#visit_expr_w () e in
+    let (++) = AtomSet.union in
 
     match e1.node with
     | EAssign ({ node = EOpen (_, atom); _ }, e1) when AtomMap.mem atom not_yet_closed ->
-        close_now_over (count e1) (fun not_yet_closed ->
+        close_now_over not_yet_closed (count e1) (fun not_yet_closed ->
           (* Combined "close now" (above) + let-binding insertion in lieu of the assignment *)
           assert (b.node.meta = Some MetaSequence);
           let e2 = snd (open_binder b e2) in
           let name, typ = AtomMap.find atom not_yet_closed in
           let b = { node = { atom; name; mut = true; mark = ref 0; meta = None }; typ } in
-          let to_close = AtomMap.remove atom not_yet_closed in
-          let e2 = self#visit_expr_w to_close (close_binder b e2) in
+          let not_yet_closed = AtomMap.remove atom not_yet_closed in
+          let e2 = self#visit_expr_w not_yet_closed (close_binder b e2) in
           ELet (b, e1, e2))
-    | EIfThenElse (e1, e2, e3) ->
+    | EIfThenElse (e, e', e'') ->
         assert (b.node.meta = Some MetaSequence);
-        close_now (AtomSet.union (count e1) (AtomSet.inter (count e2) (count e3)))
-    | EWhile (e1, _) ->
+        close_now_over not_yet_closed (
+          (* We must now bind: *)
+            (count e) ++ (* whichever variables were in the condition *)
+            (AtomSet.inter (count e') (count e'')) ++ (* variables that appear in both branches *)
+            (AtomSet.inter (count e' ++ count e'') (count e2))) (* variables in either branch *and* used later *)
+          (fun not_yet_closed ->
+            ELet (b, with_type e1.typ (EIfThenElse (self#visit_expr_w not_yet_closed e,
+              recurse_or_close not_yet_closed e',
+              recurse_or_close not_yet_closed e'')),
+              recurse_or_close not_yet_closed e2))
+    | EWhile (e, e') ->
         assert (b.node.meta = Some MetaSequence);
-        close_now (count e1)
+        close_now_over not_yet_closed (count e ++ AtomSet.inter (count e') (count e2)) (fun not_yet_closed ->
+          ELet (b,
+            with_type TUnit (EWhile (self#visit_expr_w not_yet_closed e, recurse_or_close not_yet_closed e')),
+            recurse_or_close not_yet_closed e2))
     | _ ->
         (* The open variables in e1 for which we have not yet inserted a declaration need to be closed now *)
-        close_now (count e1)
+        close_now_over not_yet_closed (count e1) (fun not_yet_closed ->
+          ELet (b, self#visit_expr_w not_yet_closed e1, recurse_or_close not_yet_closed e2))
 end
 
 let remove_units = object
@@ -93,9 +112,7 @@ end
 
 let cleanup files =
   let files = remove_units#visit_files () files in
-PPrint.(Krml.Print.(print (Krml.PrintAst.print_files files ^^ hardline)));
   let files = remove_assignments#visit_files AtomMap.empty files in
-PPrint.(Krml.Print.(print (Krml.PrintAst.print_files files ^^ hardline)));
   let files = Krml.Simplify.count_and_remove_locals#visit_files [] files in
   let files = Krml.Simplify.remove_uu#visit_files () files in
   let files = Krml.Simplify.let_to_sequence#visit_files () files in
