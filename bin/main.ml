b4181d803e54225cceb7ba1e4c02fdafbf42113e
Jonathan Protzenko <protz@microsoft.com>
2023-12-01 11:41:57 -0800

    Enable pass-by-ref

--- bin/main.ml
+++ bin/main.ml
@@ -94,6 +94,7 @@ Supported options:|}
   let files = Krml.DataTypes.simplify files in
   let files = Krml.DataTypes.optimize files in
   let _, files = Krml.DataTypes.everything files in
+  let files = Krml.Structs.pass_by_ref files in
   let files = Krml.Simplify.optimize_lets files in
   let files = Krml.Simplify.sequence_to_let#visit_files () files in
   let files = Krml.Simplify.hoist#visit_files [] files in
