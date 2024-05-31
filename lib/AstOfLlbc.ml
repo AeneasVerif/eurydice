461c2f227be15b38b60a35de04973e34c313dd84
Jonathan Protzenko <protz@microsoft.com>
2024-05-31 12:13:59 -0700

    More progress, maybe

--- lib/AstOfLlbc.ml
+++ lib/AstOfLlbc.ml
@@ -163,8 +163,10 @@ module RustNames = struct
 
   let known_builtins = [
     (* slices *)
-    parse_pattern "core::ops::index::Index<[@], core::ops::range::Range<usize>>::index", Builtin.slice_index;
-    parse_pattern "core::ops::index::IndexMut<[@], core::ops::range::Range<usize>>::index_mut", Builtin.slice_index;
+    parse_pattern "SliceIndexShared<'_, @T>", Builtin.slice_index;
+    parse_pattern "SliceIndexMut<'_, @T>", Builtin.slice_index;
+    parse_pattern "core::ops::index::Index<[@], core::ops::range::Range<usize>>::index", Builtin.slice_subslice;
+    parse_pattern "core::ops::index::IndexMut<[@], core::ops::range::Range<usize>>::index_mut", Builtin.slice_subslice;
     parse_pattern "core::slice::index::{core::ops::index::Index<[@T], @I>}::index<@, core::ops::range::Range<usize>>", Builtin.slice_subslice;
     parse_pattern "core::slice::index::{core::ops::index::IndexMut<[@T], @I>}::index_mut<@, core::ops::range::Range<usize>>", Builtin.slice_subslice;
     parse_pattern "core::slice::index::{core::ops::index::Index<[@T], @I>}::index<@, core::ops::range::RangeTo<usize>>", Builtin.slice_subslice_to;
@@ -173,6 +175,9 @@ module RustNames = struct
     parse_pattern "core::slice::index::{core::ops::index::IndexMut<[@T], @I>}::index_mut<@, core::ops::range::RangeFrom<usize>>", Builtin.slice_subslice_from;
 
     (* arrays *)
+    parse_pattern "core::ops::index::Index<[@; @], core::ops::range::Range<usize>>::index", Builtin.array_to_subslice;
+    parse_pattern "core::ops::index::IndexMut<[@; @], core::ops::range::Range<usize>>::index_mut", Builtin.array_to_subslice;
+    parse_pattern "core::array::{core::ops::index::IndexMut<[@T; @N], @I>}::index_mut<@, core::ops::range::Range<usize>, @>", Builtin.array_to_subslice;
     parse_pattern "core::array::{core::ops::index::Index<[@T; @N], @I>}::index<@, core::ops::range::Range<usize>, @>", Builtin.array_to_subslice;
     parse_pattern "core::array::{core::ops::index::IndexMut<[@T; @N], @I>}::index_mut<@, core::ops::range::Range<usize>, @>", Builtin.array_to_subslice;
     parse_pattern "core::array::{core::ops::index::Index<[@T; @N], @I>}::index<@, core::ops::range::RangeTo<usize>, @>", Builtin.array_to_subslice_to;
@@ -184,6 +189,7 @@ module RustNames = struct
     parse_pattern "ArrayToSliceShared<'_, @T, @N>", Builtin.array_to_slice;
     parse_pattern "ArrayToSliceMut<'_, @T, @N>", Builtin.array_to_slice;
     parse_pattern "core::convert::{core::convert::TryInto<@T, @U>}::try_into<&'_ [@], [@; @]>", Builtin.slice_to_array;
+    parse_pattern "core::convert::TryInto<&'_ [@], [@; @]>::try_into", Builtin.slice_to_array;
 
     (* iterators *)
     parse_pattern "core::iter::traits::collect::IntoIterator<[@; @]>::into_iter", Builtin.array_into_iter;
