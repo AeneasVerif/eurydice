b2b62fffab78f91de07c20a7fa7385242fd54a0d
Jonathan Protzenko <protz@microsoft.com>
2024-06-11 09:46:34 -0700

    And another test that needs to be fixed

--- test/closure/src/main.rs
+++ test/closure/src/main.rs
@@ -1,11 +1,13 @@
-fn f() -> [usize; 1] {
+fn f() -> [[usize; 1]; 1] {
     let s = [0; 1];
-    let a: [usize; 1] = core::array::from_fn(|i| s[0]+i);
+    let a: [[usize; 1]; 1] = core::array::from_fn(|i| {
+        core::array::from_fn(|j| (s[0]+i+j))
+    });
     a
 }
 
 fn main() {
-    let actual = f()[0];
+    let actual = f()[0][0];
     let expected = 0;
     assert_eq!(actual, expected);
 }
