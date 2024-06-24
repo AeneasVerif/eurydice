393857bdd3e84cc30430b6fecba96ac91651ec8e
Nadrieril <nadrieril+git@gmail.com>
2024-06-24 17:15:12 +0200

    Format the world

--- lib/LoadLlbc.ml
+++ lib/LoadLlbc.ml
@@ -3,7 +3,9 @@ let load_file filename =
   match Charon.LlbcOfJson.crate_of_json ic with
   | Ok r -> r
   | Error e ->
-      Printf.fprintf stderr "Error loading JSON. This is typically due to a discrepancy \
-        between charon-ml and charon. See error below. (Search for \"failed on\"). \
-        \n\n%s" e;
+      Printf.fprintf stderr
+        "Error loading JSON. This is typically due to a discrepancy between charon-ml and charon. \
+         See error below. (Search for \"failed on\"). \n\n\
+         %s"
+        e;
       exit 1
--- lib/Logging.ml
+++ lib/Logging.ml
@@ -1,18 +1,12 @@
-module StringSet = Set.Make(String)
+module StringSet = Set.Make (String)
 
-type logging =
-  | None
-  | All
-  | Some of StringSet.t
+type logging = None | All | Some of StringSet.t
 
 let logging = ref None
 
-let enable_logging (modules: string) =
+let enable_logging (modules : string) =
   let modules = String.split_on_char ',' modules in
-  if modules = [ "*" ] then
-    logging := All
-  else
-    logging := Some (StringSet.of_list modules)
+  if modules = [ "*" ] then logging := All else logging := Some (StringSet.of_list modules)
 
 let has_logging m =
   match !logging with
@@ -22,8 +16,5 @@ let has_logging m =
 
 let dummy = Buffer.create 1
 
-let log (type a) (m: string) (fmt: (a, Buffer.t, unit, unit) format4) =
-  if has_logging m then
-    Krml.KPrint.bfprintf stdout (fmt ^^ "\n")
-  else
-    Printf.ibprintf dummy fmt
+let log (type a) (m : string) (fmt : (a, Buffer.t, unit, unit) format4) =
+  if has_logging m then Krml.KPrint.bfprintf stdout (fmt ^^ "\n") else Printf.ibprintf dummy fmt
