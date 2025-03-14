a32c97328fd106d870063b67467accdf35666df5
Jonathan Protzenko <protz@microsoft.com>
2025-03-14 14:02:22 -0700

    Add tests for DSTs

---
+++ test/dst.rs
@@ -0,0 +1,18 @@
+struct S<U: ?Sized> {
+    foo: u32,
+    data: U,
+}
+
+type T = S<[u32]>;
+
+fn check(x: Box<T>) {
+    assert_eq!(x.data[0], 0);
+}
+
+fn alloc() -> Box<T> {
+    Box::new(S { foo: 0, data: [ 0; 4 ] })
+}
+
+fn main() {
+    check(alloc())
+}
