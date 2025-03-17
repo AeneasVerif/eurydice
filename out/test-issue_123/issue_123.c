364037107a528a4776b38d26c905154bb5c794ad
Nadrieril <nadrieril+git@gmail.com>
2025-03-17 17:30:38 +0100

    Update charon

--- out/test-issue_123/issue_123.c
+++ out/test-issue_123/issue_123.c
@@ -40,19 +40,20 @@ _int32_t__x2;
 void issue_123_main(void)
 {
   /* original Rust expression is not an lvalue in C */
-  ptrdiff_t lvalue0 = (ptrdiff_t)-1;
+  ptrdiff_t lvalue0 = (ptrdiff_t)((ptrdiff_t)-1 + (ptrdiff_t)0);
   /* original Rust expression is not an lvalue in C */
   ptrdiff_t lvalue1 = (ptrdiff_t)-1;
   _ptrdiff_t__x2 uu____0 = { .fst = &lvalue0, .snd = &lvalue1 };
   ptrdiff_t *left_val0 = uu____0.fst;
   ptrdiff_t *right_val0 = uu____0.snd;
   EURYDICE_ASSERT(left_val0[0U] == right_val0[0U], "panic!");
-  int32_t uu____1 = issue_123_fun(issue_123_E_One);
+  /* original Rust expression is not an lvalue in C */
+  int32_t lvalue2 = issue_123_fun(issue_123_E_One);
   /* original Rust expression is not an lvalue in C */
   int32_t lvalue = (int32_t)1;
-  _int32_t__x2 uu____2 = { .fst = &uu____1, .snd = &lvalue };
-  int32_t *left_val = uu____2.fst;
-  int32_t *right_val = uu____2.snd;
+  _int32_t__x2 uu____1 = { .fst = &lvalue2, .snd = &lvalue };
+  int32_t *left_val = uu____1.fst;
+  int32_t *right_val = uu____1.snd;
   EURYDICE_ASSERT(left_val[0U] == right_val[0U], "panic!");
 }
 
