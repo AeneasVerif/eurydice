0d7c5e538de51c90dca910f9499edb97f990589c
Jonathan Protzenko <protz@microsoft.com>
2025-05-12 08:43:01 -0400

    Better test

--- test/dst.rs
+++ test/dst.rs
@@ -14,6 +14,18 @@ fn alloc() -> Box<T> {
     Box::new(S { foo: 0, data: [ 0; 4 ] })
 }
 
+struct T2<U: ?Sized> {
+    header: usize,
+    data: U,
+}
+
+fn mk() -> Box<T2<[u32]>> {
+    let x = T2 { header: 0, data: [0u32; 4] };
+    let y: Box<T2<[u32]>> = Box::new(x);
+    y
+}
+
 fn main() {
-    check(alloc())
+    check(alloc());
+    assert_eq!((mk()).data[0], 0);
 }
