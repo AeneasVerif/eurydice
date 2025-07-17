9884ca1b222c273c6222d59133fcb21d2cf48e81
Jonathan Protzenko <protz@microsoft.com>
2025-07-17 14:26:51 -0700

    Greatly simplify test

--- test/parentparent.rs
+++ test/parentparent.rs
@@ -1,18 +1,14 @@
-trait FnMut<F, T>: FnOnce<F, T> {
-    fn call_mut(&mut self, arg: F) -> T;
+trait A: B {
 }
 
-trait FnOnce<F, T>: Foo<F, T> {
-    fn call_once(&mut self, arg: F) -> T;
+trait B: C {
 }
 
-trait Foo<F, T> {
-    fn foo(&mut self, arg: F) -> T;
+trait C {
+    fn foo(&mut self);
 }
 
-fn f<F, T, U: FnMut<F, T>>(mut x: U, y: F) -> T {
-    x.call_mut(y)
+fn f<U: A> (x: U) {
 }
 
-fn main() {
-}
+fn main() {}
