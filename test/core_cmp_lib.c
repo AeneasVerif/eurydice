14c5b2078038c47a636e16f30c31d06493b55891
Ling Zhang <ling.zhang@sjtu.edu.cn>
2025-09-17 15:35:33 +0800

    minor: recorded different name for partial_eq

--- test/core_cmp_lib.c
+++ test/core_cmp_lib.c
@@ -7,3 +7,8 @@
 bool core_cmp_impls__core__cmp__PartialEq_____for_____eq(void **x0, void **x1) {
   return true;
 }
+
+// same implementation for the monomorphized LLBC
+bool core_cmp_impls__core__cmp__PartialEq____________eq(void **x0, void **x1) {
+  return true;
+}
